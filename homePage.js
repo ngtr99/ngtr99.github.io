0479ced03b0f1ce6d2b06874a46b671bba08c608
Nguyệt Trang <nguyet@Nguyets-Laptop.local>
2025-03-17 00:52:05 -0700

    add infoPage.html, contactPage.html, and projectPage.html

--- homePage.js
+++ homePage.js
@@ -6,7 +6,7 @@ let index = 0;
 function updateTitle() {
     title.style.fontSize = '2rem';
     title.style.color = 'white';
-    title.textContent = 'Hello, I am a ' +  titleText[index];
+    title.textContent = 'Hello, I am a ' + titleText[index];
     title.style.transition = 'all 0.5s';
     index = (index + 1) % titleText.length;
     setTimeout(updateTitle, 1000);
