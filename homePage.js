38d75e1d45c156438b40a30f4496d02af0444a16
Nguyệt Trang <nguyet@Nguyets-Laptop.local>
2025-03-16 18:36:49 -0700

    upload the pic

---
+++ homePage.js
@@ -0,0 +1,16 @@
+// Class Container
+const title = document.querySelector('.title');
+let titleText = ['student', 'web developer', 'freelancer', 'designer'];
+
+let index = 0;
+function updateTitle() {
+    title.style.fontSize = '2rem';
+    title.style.color = 'white';
+    title.textContent = 'Hello, I am a ' +  titleText[index];
+    title.style.transition = 'all 0.5s';
+    index = (index + 1) % titleText.length;
+    setTimeout(updateTitle, 1000);
+}
+
+updateTitle();
+
